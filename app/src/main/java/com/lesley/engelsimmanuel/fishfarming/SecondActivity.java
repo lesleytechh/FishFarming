3a51d576dec71525e2818a901cf6b92e6600f365
acer <lesleychiamakaa94@gmail.com>
2020-08-31 22:27:21 +0100

    Designed Layouts

---
+++ app/src/main/java/com/lesley/engelsimmanuel/fishfarming/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.lesley.engelsimmanuel.fishfarming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
